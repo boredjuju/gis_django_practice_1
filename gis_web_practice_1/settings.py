f01e318d47ce5155846491c8f7f3fdbfc652235f
Hyeonju Cha <hy9498@gmail.com>
2021-07-13 16:22:26 +0900

    7/12 django practice 1

--- gis_web_practice_1/settings.py
+++ gis_web_practice_1/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 local_env = open(os.path.join(BASE_DIR, '.env'))
@@ -142,3 +145,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
